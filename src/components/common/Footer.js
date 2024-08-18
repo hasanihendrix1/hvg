34d88091c36d0487ecbb09879d40b5d12219c14d
Hasani Hendrix <hasanihendrix1@gmail.com>
2024-08-18 02:18:58 -0400

    Initial page updates

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -16,10 +16,10 @@ const Footer = () => {
 
       <div className="footer-description">
         <p>
-          We are a driven real estate solutions firm, helping homeowners
+          We are a real estate solutions firm, helping homeowners
           overcome property challenges quickly and effortlessly. Specializing in
           fair, all-cash offers, we streamline every deal for a stress-free
-          experience. Powered by coffee and passion, we pride ourselves on being
+          experience. We pride ourselves on being
           the cream of the crop, uplifting communities one transaction at a
           time.
         </p>
@@ -30,6 +30,7 @@ const Footer = () => {
           © {new Date().getFullYear()} Hendrix Ventures Group, LLC. All rights
           reserved.
         </p>
+        <p>- Powered by coffee and passion</p>
       </div>
     </footer>
   );
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css'
-import App from './App';
-import reportWebVitals from './utils/reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.css";
+import App from "./App";
+import reportWebVitals from "./utils/reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
