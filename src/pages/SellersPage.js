3cdfafc3b22955e0d43af341fce4f26c75680b1d
Hasani Hendrix <hasanihendrix1@gmail.com>
2024-08-17 23:13:52 -0400

    Minor styling updates

--- src/pages/SellersPage.js
+++ src/pages/SellersPage.js
@@ -7,20 +7,20 @@ import WaveSVG from "../assets/images/hero-divider.svg";
 
 const SellersPage = () => {
   return (
-      <div className="sellers-page">
-        {/* Hero and Form Sections Container */}
-          <div className="hero-form-container">
-              <img src={WaveSVG} alt="wave divider" className="wave-svg" />
-          <HeroSection
-            title="We Buy Houses in Atlanta, FAST!"
-            subtitle={`No Fees | No Commissions | No Closing Costs\nAny Condition Accepted | Get a Net Cash Offer\nSkip the Hassle – Sell Direct, Get Paid Faster.\n\nThis is Hendrix Ventures Group, LLC Official Website`}
-              />
+    <div className="sellers-page">
+      {/* Hero and Form Sections Container */}
+      <div className="hero-form-container">
+        <img src={WaveSVG} alt="wave divider" className="wave-svg" />
+        <HeroSection
+          title="We Buy Houses in Atlanta, FAST!"
+          subtitle={`No Fees | No Commissions | No Closing Costs\nAny Condition Accepted | Get a Net Cash Offer\nSkip the Hassle – Sell Direct, Get Paid Faster.\n\nThis is Hendrix Ventures Group, LLC Official Website`}
+        />
 
-          <SellerForm />
-        </div>
-        {/* Overview Section */}
-        <OverviewSection />
+        <SellerForm />
       </div>
+      {/* Overview Section */}
+      <OverviewSection />
+    </div>
   );
 };
 
