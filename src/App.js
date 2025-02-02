94122dddc63e3bdee6ff8d69c169a9326f65a8ca
Hasani Hendrix <hasanihendrix1@gmail.com>
2025-02-01 22:02:20 -0500

    coming soon page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SellersPage from "./pages/SellersPage";
 import BuyersPage from "./pages/BuyersPage";
+import ComingSoonPage from "./pages/ComingSoonPage"; // New page
 import BuyersNav from "./components/common/BuyerNav";
 import BuyerFooter from "./components/common/BuyerFooter";
 
@@ -15,6 +11,7 @@ function App() {
     <Router>
       <BuyersNav />
       <Routes>
+        <Route path="/" element={<ComingSoonPage />} /> {/* Landing Page */}
         <Route path="/investordeals" element={<BuyersPage />} />
       </Routes>
       <BuyerFooter />
---
+++ src/pages/ComingSoonPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/pages/ComingSoonPage.css";
+
+function ComingSoonPage() {
+  return (
+    <div className="coming-soon-container">
+      <h1 className="coming-soon-title">Hendrix Ventures</h1>
+      <p className="coming-soon-text">Coming Soon! Stay tuned.</p>
+      <button className="notify-button">Notify Me</button> {/* Optional */}
+    </div>
+  );
+}
+
+export default ComingSoonPage;
